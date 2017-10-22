88b7dd1be3deb9098113849f0dbdb7b566eb5674
Mac <mac@Macs-MacBook-Pro.local>
2017-10-22 21:46:39 +0400

    Inited Android Project

---
+++ app/src/main/java/tic_tac_toe/muh_azharudheen/com/tic_tac_toe/MainActivity.kt
@@ -0,0 +1,12 @@
+package tic_tac_toe.muh_azharudheen.com.tic_tac_toe
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
